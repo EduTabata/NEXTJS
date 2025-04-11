1f4cc87ce65ba8dcbba22cce59abc5d6dd8b9e56
EduTabata <dudutabata@gmail.com>
2025-04-11 12:23:01 -0300

    Update next.config.ts

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from 'next';
 
+/** @type {NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Suas configurações personalizadas aqui (exemplo: React Strict Mode)
+  reactStrictMode: true,
+
+  // Redirecionamentos
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
